3c6a4801bbd6490f256da9b5175c6a924ab47017
DoubledDoge <dbritz22@proton.me>
2026-09-21 12:35:10 +0200

    fix: Removed redundant potentially overlapping statuses

--- splithappens/src/main/java/io/github/doubleddoge/splithappens/data/enums/HandStatus.kt
+++ splithappens/src/main/java/io/github/doubleddoge/splithappens/data/enums/HandStatus.kt
@@ -1,10 +1,9 @@
 package io.github.doubleddoge.splithappens.data.enums
 
+// Split can instead be determined if another hand points at it through parentHandId
 enum class HandStatus {
-	ACTIVE, // Might never reach database, rounds written at the end
 	STOOD,
 	BUSTED,
 	BLACKJACK,
 	SURRENDERED,
-	SPLIT // Could overlap with child hands
 }
