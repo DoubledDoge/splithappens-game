a03f68f750f23f1d101a5bf3d7812d416ccd58ee
DoubledDoge <dbritz22@proton.me>
2026-09-21 12:01:59 +0200

    feat: Reworked HandResult to offer different payouts

--- splithappens/src/main/java/io/github/doubleddoge/splithappens/data/model/HandResult.kt
+++ splithappens/src/main/java/io/github/doubleddoge/splithappens/data/model/HandResult.kt
@@ -1,8 +1,16 @@
 package io.github.doubleddoge.splithappens.data.model
 
-enum class HandResult {
-	IN_PROGRESS, // Might never reach database, rounds written at the end
-	WIN,
-	LOSS,
-	PUSH
+// Has a multiplier to enable different payouts decided by game logic
+enum class HandResult(val multiplier: Int) {
+	LOSS(0),
+	PUSH(1),                // Tie
+	WIN(2),
+	BLACKJACK(3),           // ace + a ten-value card
+	QUEEN_JACK(10),         // queen + ace
+	NATURAL_BLACKJACK(50),  // jack + ace
+	TRIPLE_SEVEN(100),      // 21 with three 7s
+	SIX_CARD(120),          // six cards without busting
+	SEVEN_CARD(250);        // seven cards without busting
+
+	fun payoutFor(bet: Long): Long = bet * multiplier
 }
