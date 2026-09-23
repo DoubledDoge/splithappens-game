02f5ef7ae57a9b9c15ad261ed6893c97ea9ccb69
DoubledDoge <dbritz22@proton.me>
2026-09-23 12:43:12 +0200

    fix: Fixed build issues

--- splithappens/build.gradle.kts
+++ splithappens/build.gradle.kts
@@ -1,60 +1,61 @@
 plugins {
-	alias(libs.plugins.android.application)
-	alias(libs.plugins.kotlin.compose)
-	alias(libs.plugins.ksp)
-	alias(libs.plugins.androidx.room3)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.androidx.room3)
 }
 
 android {
-	namespace = "io.github.doubleddoge.splithappens"
-	compileSdk {
-		version = release(37)
-	}
+    namespace = "io.github.doubleddoge.splithappens"
+    compileSdk {
+        version = release(37)
+    }
 
-	defaultConfig {
-		applicationId = "io.github.doubleddoge.splithappens"
-		minSdk = 29
-		targetSdk = 37
-		versionCode = 1
-		versionName = "1.0"
-		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-	}
+    defaultConfig {
+        applicationId = "io.github.doubleddoge.splithappens"
+        minSdk = 29
+        targetSdk = 37
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
 
-	buildTypes {
-		release {
-			optimization {
-				enable = false
-			}
-		}
-	}
-	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_11
-		targetCompatibility = JavaVersion.VERSION_11
-	}
-	buildFeatures {
-		compose = true
-	}
+    buildTypes {
+        release {
+            optimization {
+                enable = false
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+    }
 }
 
-room3{
-	schemaDirectory("$projectDir/schemas")
+room3 {
+    schemaDirectory("$projectDir/schemas")
 }
 
 dependencies {
-	implementation(platform(libs.androidx.compose.bom))
-	implementation(libs.androidx.activity.compose)
-	implementation(libs.androidx.compose.material3)
-	implementation(libs.androidx.compose.ui)
-	implementation(libs.androidx.compose.ui.graphics)
-	implementation(libs.androidx.compose.ui.tooling.preview)
-	implementation(libs.androidx.core.ktx)
-	implementation(libs.androidx.lifecycle.runtime.ktx)
-	implementation(libs.androidx.room3.runtime)
-	ksp(libs.androidx.room3.compiler)
-	testImplementation(libs.junit)
-	androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-	androidTestImplementation(libs.androidx.espresso.core)
-	androidTestImplementation(libs.androidx.junit)
-	debugImplementation(libs.androidx.compose.ui.test.manifest)
-	debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.room3.runtime)
+    ksp(libs.androidx.room3.compiler)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    debugImplementation(libs.androidx.compose.ui.tooling)
 }
